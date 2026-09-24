6b5990b9f6c64d3a9c3de0a1c5173b68935d0301
thestandardinst-star <thestandardinst@gmail.com>
2026-09-24 18:24:26 +0000

    Design Wave 1: load canonical foundation after compatibility styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,10 @@ import "./premium-staff.css";
 import "./premium-manager.css";
 import "./premium-admin.css";
 import "./premium-executive.css";
+import "./styles/tokens.css";
+import "./styles/type.css";
+import "./styles/motion.css";
+import "./styles/base.css";
+import "./styles/primitives.css";
 
 createRoot(document.getElementById("root")).render(<React.StrictMode><App /></React.StrictMode>);
